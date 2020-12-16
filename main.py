83862527c8f7a8c440f697716549a90e5d3969fb
skyleaworlder <870033938@qq.com>
2020-12-17 00:04:36 +0800

    M: add output formatting about file-output

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from factory import Instr_Factory
-from utils import separate, trans
+from tools.factory import Instr_Factory
+from tools.utils import separate, trans
 import argparse
+from math import log10, ceil
 
 '''
 input: file_path
@@ -10,15 +11,18 @@ def main(mode: str, infile_path: str, outfile_path=None):
     assert not (mode == "FILE" and outfile_path is None)
 
     fin = open(infile_path)
-    lines = fin.readlines()
-    lines = [int(line.strip('\n'), 16) for line in lines]
+    origin_lines = fin.readlines()
+    origin_lines = [int(line.strip('\n'), 16) for line in origin_lines]
 
     if mode == "FILE":
         fout = open(outfile_path, "w", encoding="utf-8")
 
-    sepa_lst = [separate(line) for line in lines]
+    # get instruction type
+    sepa_lst = [separate(line) for line in origin_lines]
     trans_lst = [trans(line) for line in sepa_lst]
     instr_txt_lst = []
+
+    # process instructions input
     for line in trans_lst:
         instr = line[0]
         instr_fact = Instr_Factory(
@@ -30,29 +34,45 @@ def main(mode: str, infile_path: str, outfile_path=None):
     if mode == "FILE":
         # add '\n'
         instr_txt_lst = [instr_txt+'\n' for instr_txt in instr_txt_lst]
-        fout.writelines(instr_txt_lst)
+
+        # thanks to https://stackoverflow.com/questions/11676864/how-can-i-format-an-integer-to-a-two-digit-hex/11677120
+        #       and https://note.nkmk.me/en/python-for-enumerate-zip/
+        #       and https://pyformat.info/ (about formats positional arguments)
+        instr_num_lg = ceil(log10(len(instr_txt_lst)))
+        line_head_len = max(instr_num_lg, len("idx"))
+        fout.write(
+            "{:{}{}}".format("idx", '>', line_head_len)+"    "
+            +"{:{}{}}".format("hex code", '>', 10)+"    "
+            +"{:{}}".format("mips-assembly", '<')+'\n'
+        )
+        # add breakline
+        fout.write("-"*40+'\n')
+        for idx, (origin_hex, instr_txt) in enumerate(zip(origin_lines, instr_txt_lst)):
+            fout.write("{:{}{}}".format(idx, '>', line_head_len)+"    "+"0x{:08x}".format(origin_hex)+"    "+instr_txt)
+
     elif mode == "STD":
-        for instr_txt in instr_txt_lst:
+        for origin_hex, instr_txt in zip(origin_lines, instr_txt_lst):
             print(instr_txt)
     else:
         pass
 
-parser = argparse.ArgumentParser()
-
-choices = [
-    { "method": "-d", "vmethod": "--dsasmb", "help": "disassemble *.txt" },
-    { "method": "-o", "vmethod": "--output", "help": "output to *.txt" }
-]
-
-for choice in choices:
-    parser.add_argument(
-        choice["method"], choice["vmethod"], type=str,
-        help=choice["help"]
-    )
-
-args = parser.parse_args()
-
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+
+    choices = [
+        { "method": "-d", "vmethod": "--dsasmb", "help": "disassemble *.txt" },
+        { "method": "-o", "vmethod": "--output", "help": "output to *.txt" }
+    ]
+
+    for choice in choices:
+        parser.add_argument(
+            choice["method"], choice["vmethod"], type=str,
+            help=choice["help"]
+        )
+
+    args = parser.parse_args()
+
     infile_path = args.dsasmb
     outfile_path = args.output
 
